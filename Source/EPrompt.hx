39fb9abbf61872d6fda0392ea092aa36e7e84b72
Nick Caston <ntcaston@gmail.com>
2020-02-04 13:16:47 +1100

    tabs -> spaces

--- Source/EPrompt.hx
+++ Source/EPrompt.hx
@@ -6,22 +6,22 @@ import openfl.geom.Rectangle;
 import openfl.geom.Point;
 
 class EPrompt extends Sprite {
-	public static inline var Width = 25;
-	public static inline var Height = 7;
+  public static inline var Width = 25;
+  public static inline var Height = 7;
 
-	var spriteSheet:BitmapData;
-	var sprite:Bitmap;
+  var spriteSheet:BitmapData;
+  var sprite:Bitmap;
 
-	public function new() {
-		super();
-		this.spriteSheet = Assets.getBitmapData("Assets/press_e.png");
-		this.sprite = new Bitmap(new BitmapData(Width, Height, true, 0xFFFFFFFF));
-		addChild(this.sprite);
-		this.onFrame(0);
-	}
+  public function new() {
+    super();
+    this.spriteSheet = Assets.getBitmapData("Assets/press_e.png");
+    this.sprite = new Bitmap(new BitmapData(Width, Height, true, 0xFFFFFFFF));
+    addChild(this.sprite);
+    this.onFrame(0);
+  }
 
-	public function onFrame(frame:Int) {
-		var index = frame % 2;
-		this.sprite.bitmapData.copyPixels(this.spriteSheet, new Rectangle(0, index * Height, Width, Height), new Point(0, 0));
-	}
+  public function onFrame(frame:Int) {
+    var index = frame % 2;
+    this.sprite.bitmapData.copyPixels(this.spriteSheet, new Rectangle(0, index * Height, Width, Height), new Point(0, 0));
+  }
 }
--- Source/Sky.hx
+++ Source/Sky.hx
@@ -19,74 +19,74 @@ import openfl.media.SoundTransform;
 import openfl.utils.Assets;
 
 class Sky extends Sprite {
-	public static inline var WhiteColor = 0xc7f0d8;
-	public static inline var BlackColor = 0x43523d;
+  public static inline var WhiteColor = 0xc7f0d8;
+  public static inline var BlackColor = 0x43523d;
 
-	public static inline var Width = 84;
-	public static inline var Height = 48;
+  public static inline var Width = 84;
+  public static inline var Height = 48;
 
-	public static inline var StarRows = 0;
-	public static inline var StarCols = 0;
+  public static inline var StarRows = 0;
+  public static inline var StarCols = 0;
 
-	var cloudNoise:BitmapData;
+  var cloudNoise:BitmapData;
 
-	var eprompt:EPrompt;
+  var eprompt:EPrompt;
 
-	public function new() {
-		super();
+  public function new() {
+    super();
 
-		this.shader = new NokiaShader();
+    this.shader = new NokiaShader();
 
-		this.graphics.beginFill(BlackColor);
-		this.graphics.drawRect(0, 0, Width, Height);
+    this.graphics.beginFill(BlackColor);
+    this.graphics.drawRect(0, 0, Width, Height);
 
-		// Generate star field
-		var star = new Sprite();
-		for (i in 0...(StarRows * StarCols)) {
-			star.graphics.beginFill(WhiteColor);
-			var row = i / StarRows;
-			var col = i % StarRows;
-			var x = Math.round(col * Width / StarRows + Math.random() * Width / StarRows);
-			var y = Math.round(row * Height / StarCols + Math.random() * Height / StarCols);
-			star.graphics.drawRect(x, y, 1, 1);
-		};
-		addChild(star);
+    // Generate star field
+    var star = new Sprite();
+    for (i in 0...(StarRows * StarCols)) {
+      star.graphics.beginFill(WhiteColor);
+      var row = i / StarRows;
+      var col = i % StarRows;
+      var x = Math.round(col * Width / StarRows + Math.random() * Width / StarRows);
+      var y = Math.round(row * Height / StarCols + Math.random() * Height / StarCols);
+      star.graphics.drawRect(x, y, 1, 1);
+    };
+    addChild(star);
 
-		// Crescent moon moon
-		var moon = new Sprite();
-		moon.graphics.beginFill(WhiteColor);
-		moon.graphics.drawCircle(15, 15, 10);
-		moon.graphics.beginFill(BlackColor);
-		moon.graphics.drawCircle(10, 13, 9.5);
-		addChild(moon);
+    // Crescent moon moon
+    var moon = new Sprite();
+    moon.graphics.beginFill(WhiteColor);
+    moon.graphics.drawCircle(15, 15, 10);
+    moon.graphics.beginFill(BlackColor);
+    moon.graphics.drawCircle(10, 13, 9.5);
+    addChild(moon);
 
-		// Cloud sprite - a layer of perlin noise
-		this.cloudNoise = new BitmapData(84, 48, true, WhiteColor);
-		cloudNoise.perlinNoise(0, -10, 2, 1, false, false, BitmapDataChannel.ALPHA, true);
-		var cloud:Sprite = new Sprite();
-		cloud.addChild(new Bitmap(cloudNoise));
-		cloud.shader = new CloudShader();
-		// Lower cutoff = smaller clouds
-		cloud.shader.data.cutoff.value = [0.35];
-		// How much the moonlight highlights clouds in x/y direction
-		cloud.shader.data.lightMul.value = [1, 2];
-		addChild(cloud);
+    // Cloud sprite - a layer of perlin noise
+    this.cloudNoise = new BitmapData(84, 48, true, WhiteColor);
+    cloudNoise.perlinNoise(0, -10, 2, 1, false, false, BitmapDataChannel.ALPHA, true);
+    var cloud:Sprite = new Sprite();
+    cloud.addChild(new Bitmap(cloudNoise));
+    cloud.shader = new CloudShader();
+    // Lower cutoff = smaller clouds
+    cloud.shader.data.cutoff.value = [0.35];
+    // How much the moonlight highlights clouds in x/y direction
+    cloud.shader.data.lightMul.value = [1, 2];
+    addChild(cloud);
 
-		var titleData = Assets.getBitmapData("Assets/title.png");
-		var title = new Bitmap(titleData);
-		title.x = 27;
-		title.y = 10;
-		addChild(title);
-	}
+    var titleData = Assets.getBitmapData("Assets/title.png");
+    var title = new Bitmap(titleData);
+    title.x = 27;
+    title.y = 10;
+    addChild(title);
+  }
 
-	public function onFrame(frame) {
-		cloudNoise.perlinNoise(Math.floor(frame / 2), -10, 2, 1, false, false, BitmapDataChannel.ALPHA, true);
-		if (frame > 15) {
-			eprompt = new EPrompt();
-			eprompt.x = 84 - 26;
-			eprompt.y = 48 - 8;
-			addChild(eprompt);
-			eprompt.onFrame(1);
-		}
-	}
+  public function onFrame(frame) {
+    cloudNoise.perlinNoise(Math.floor(frame / 2), -10, 2, 1, false, false, BitmapDataChannel.ALPHA, true);
+    if (frame > 15) {
+      eprompt = new EPrompt();
+      eprompt.x = 84 - 26;
+      eprompt.y = 48 - 8;
+      addChild(eprompt);
+      eprompt.onFrame(1);
+    }
+  }
 }
