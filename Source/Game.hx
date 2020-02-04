8d9199bbefa9d2338f0b26ac118e5e6661a22f2e
Callan McNamara <callan.mcnamara@gmail.com>
2020-02-04 16:14:58 +1100

    Dampe status bar overlay

--- Source/Game.hx
+++ Source/Game.hx
@@ -148,7 +148,7 @@ class Game extends Sprite {
     var dampeRect = Dampe.localSpaceMovementCollider().clone();
     dampeRect.offset(x, y);
     if (dampeRect.x < 0
-      || dampeRect.y < 5
+      || dampeRect.y < 11
       || (dampeRect.x + dampeRect.width > Width)
       || (dampeRect.y + dampeRect.height > Height)) {
       return false;
@@ -240,7 +240,7 @@ class Game extends Sprite {
     this.statusBar.onFrame(1 - (frame / DayFrames));
   }
 
-  private function getChildAtCell(cell:Cell): DisplayObject {
+  private function getChildAtCell(cell:Cell):DisplayObject {
     for (i in 0...numChildren) {
       var child = getChildAt(i);
       if (cellHelper.getClosestCell(child.x, child.y).isSameAs(cell)) {
@@ -291,7 +291,7 @@ class Game extends Sprite {
 
   // Painters algo. Tombstones + Dampe are sorted based on "base y" so dampe is
   // behind some tombstones and in front of others.
-  // Ghosts are on top of everything.
+  // Ghosts and status bar are on top of everything.
   function sortChildren() {
     final tombstoneRect = Tombstone.localSpacePathingCollider();
     final dampeRect = Dampe.localSpaceMovementCollider();
@@ -305,6 +305,8 @@ class Game extends Sprite {
           return child.y + dampeRect.y + dampeRect.height;
         case Ghost:
           return 999 + child.y;
+        case StatusBar:
+          return 10000 + child.y;
         default:
           return -1;
       }
